f6e4aa054cbd63da6906255b9d0859a6a20f85fc
Jared McCoon <97768332+jaredmccoon@users.noreply.github.com>
2022-02-01 13:27:39 -0800

    Add files via upload

---
+++ Likes/Likes1/script.js
@@ -0,0 +1,6 @@
+let likes = 0;
+
+function count(element) {
+    likes += 1;
+    document.getElementById("add").innerHTML = likes
+}
---
+++ Likes/Likes2/script.js
@@ -0,0 +1,18 @@
+let likes1 = 0;
+let likes2 = 0;
+let likes3 = 0;
+function count1(element) {
+    likes1++;
+    document.getElementById("add1_1").innerHTML = likes1
+}
+
+function count2(element) {
+    likes2++;
+    document.getElementById("add1_2").innerHTML = likes2
+}
+
+function count3(element) {
+    likes3++;
+    document.getElementById("add1_3").innerHTML = likes3
+}
+
